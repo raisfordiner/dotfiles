64eb669acc8c0ef59a0bb1add8b3423545a09d68
Rice4Dinner2Go <truongkienthanh6705@gmail.com>
2025-02-21 14:17:29 +0700

    Follow harpoon man!

--- nvim/.config/nvim/lua/plugins/navigation-stuffs.lua
+++ nvim/.config/nvim/lua/plugins/navigation-stuffs.lua
@@ -12,4 +12,42 @@ return {
             -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
         },
 	},
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local harpoon = require("harpoon")
+
+			-- REQUIRED
+			harpoon:setup()
+			-- REQUIRED
+
+			vim.keymap.set("n", "<leader>h", function()
+				harpoon:list():add()
+			end)
+			vim.keymap.set("n", "<leader>H", function()
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end)
+
+			vim.keymap.set("n", "<C-1>", function()
+				harpoon:list():select(1)
+			end)
+			vim.keymap.set("n", "<C-2>", function()
+				harpoon:list():select(2)
+			end)
+			vim.keymap.set("n", "<C-3>", function()
+				harpoon:list():select(3)
+			end)
+			vim.keymap.set("n", "<C-4>", function()
+				harpoon:list():select(4)
+			end)
+			vim.keymap.set("n", "<C-5>", function()
+				harpoon:list():select(5)
+			end)
+			vim.keymap.set("n", "<C-6>", function()
+				harpoon:list():select(6)
+			end)
+		end,
+	},
 }
