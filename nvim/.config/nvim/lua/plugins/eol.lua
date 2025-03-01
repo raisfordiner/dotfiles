59e6f12c010c71a097419b54e17b90b8a6d50fd5
Rice4Dinner2Go <truongkienthanh6705@gmail.com>
2025-03-01 14:15:58 +0700

    Update neovim and zsh

--- nvim/.config/nvim/lua/plugins/eol.lua
+++ nvim/.config/nvim/lua/plugins/eol.lua
@@ -13,11 +13,9 @@ return {
 		end,
 	},
 	{
-		"kylechui/nvim-surround",
-		version = "*", -- Use for stability; omit to use `main` branch for the latest features
-		event = "VeryLazy",
+		"ricefordinr/yet-another-surround.nvim",
 		config = function()
-			require("nvim-surround").setup({})
+			require("surround").setup()
 		end,
 	},
 	{
