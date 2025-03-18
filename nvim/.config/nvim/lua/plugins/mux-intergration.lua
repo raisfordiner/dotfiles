9acf8ef67ec27b3ecfa12c7cd14858959a9e9041
Rice4Dinner2Go <truongkienthanh6705@gmail.com>
2025-03-18 09:21:54 +0700

    feat: Zellij intergration for neovim

--- nvim/.config/nvim/lua/plugins/mux-intergration.lua
+++ nvim/.config/nvim/lua/plugins/mux-intergration.lua
@@ -17,4 +17,16 @@ return {
 			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
 		},
 	},
+	{
+		"swaits/zellij-nav.nvim",
+		lazy = true,
+		event = "VeryLazy",
+		keys = {
+			{ "<c-h>", "<cmd>ZellijNavigateLeftTab<cr>", { silent = true, desc = "navigate left or tab" } },
+			{ "<c-j>", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
+			{ "<c-k>", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
+			{ "<c-l>", "<cmd>ZellijNavigateRightTab<cr>", { silent = true, desc = "navigate right or tab" } },
+		},
+		opts = {},
+	},
 }
